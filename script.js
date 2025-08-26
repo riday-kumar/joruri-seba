f121bccffdbb407abfece4acf1c6d6d6490f0f31
riday-kumar <hridoykumarsaha07@gmail.com>
2025-08-27 02:50:03 +0600

    content changed and increase the count when click heart icon functionality added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("aschi");
+function elementId(id) {
+  const catchId = document.getElementById(id);
+  return catchId;
+}
+
+function elementClass(clsName) {
+  let catchClasses = document.getElementsByClassName(clsName);
+  return catchClasses;
+}
+
+elementId("emergencyNumbers").addEventListener("click", function (e) {
+  // increase Love in the nev bar
+  const checkHeart = e.target.className;
+  if (checkHeart.includes("heart")) {
+    let initialLove = Number(elementId("love").innerText);
+    initialLove++;
+    console.log(initialLove);
+    elementId("love").innerText = initialLove;
+  }
+});
